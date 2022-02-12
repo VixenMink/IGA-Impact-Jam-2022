224a98737885a4903e3b20d2ca4d1f6489bed9d7
zaknafean <zaknafean@gmail.com>
2022-02-12 07:56:17 -0500

    Simulation: Cleanup
    Cleaning up things I don't think we'll be using

---
+++ space-wolves/wildlife/MovementStates/RandomMoveState.gd
@@ -0,0 +1,56 @@
+extends EnemyBaseState
+
+var movetimer_length = 35
+var movetimer = 0
+
+
+#State when enemy is waiting to find something
+func _ready():
+	 nameLabel = 'Random'
+
+
+func _get_input(_event):
+	pass
+
+
+func _state_logic(_delta):
+	if movetimer > 0:
+		movetimer -= 1
+	if movetimer == 0 or selfRef.is_on_wall() or selfRef.global_position.distance_to(selfRef.origin_pos) > 50:
+		selfRef.move_dir = random_direction()
+		movetimer = movetimer_length
+	
+	var randoVelocity = selfRef.move_dir.normalized()
+	
+	var _err = selfRef._apply_momentum(randoVelocity, selfRef.SPEED)
+
+
+func _get_transition(_delta, _states_map):
+	
+	return null
+
+
+func _enter_state(_new_state, _old_state):
+	#animationRef.play("default")
+	selfRef.move_dir = random_direction()
+
+
+func _exit_state(_old_state, _new_state):
+	pass
+
+
+func _on_anim_complete(_anim_name):
+	pass
+
+
+func random_direction():
+	randomize()
+	match (randi() % 4):
+		0:
+			return Vector2.LEFT
+		1:
+			return Vector2.RIGHT
+		2:
+			return Vector2.UP
+		3:
+			return Vector2.DOWN
