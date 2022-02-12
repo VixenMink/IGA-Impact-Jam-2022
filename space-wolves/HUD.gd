d29f01491325dc1e19f2566081bb18404d4eaae0
PickyBurrito49 <83426261+PickyBurrito49@users.noreply.github.com>
2022-02-12 11:58:49 -0600

    Canvas layer
    HUD new Scene

---
+++ space-wolves/HUD.gd
@@ -0,0 +1,8 @@
+extends CanvasLayer
+
+onready var Cash = $MarginContainer/BaseBox/TopBox/VBoxContainer/Cash
+onready var PredatorPop = $MarginContainer/BaseBox/TopBox/VBoxContainer/Cash
+onready var PreyPop = $"MarginContainer/BaseBox/TopBox/VBoxContainer/Predator pop"
+onready var ResoursePop = $"MarginContainer/BaseBox/TopBox/VBoxContainer/Resourse Pop"
+onready var Progress = $MarginContainer/BaseBox/TopBox/VBoxContainer2/ProgressBar
+onready var Round = $MarginContainer/BaseBox/TopBox/VBoxContainer2/Round
